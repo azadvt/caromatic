9a6c99db0c1ca81e959d2ea79f0e4191dfc2a70f
azadvt <azad.vt@outlook.com>
2022-11-12 20:10:35 +0530

    initial commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export function NavbarTop() {
+  return (
+    <div>NavbarTop</div>
+  )
+}
+
+  
+
+
+export function Footer() {
+    return (
+      <div>Footer</div>
+    )
+  }
+  
+    
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Footer, NavbarTop } from '../components/Navigation/Navigation'
+
+function Home() {
+  return (
+    <div>
+        <NavbarTop/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
