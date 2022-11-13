2535e9eb8dcfb98b186ca2ccbf9cbc6088383000
azadvt <azad.vt@outlook.com>
2022-11-13 11:03:46 +0530

    signup page designed

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/user/Home";
 import Login from "../pages/user/Login";
+import Signup from "../pages/user/Signup";
 function UserRoute() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
         <Route path="*" element={<p>Path not resolved</p>} />
 
       </Routes>
